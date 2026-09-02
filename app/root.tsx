ec643a75d56332b0b2c2640d8c627ab5c7579132
AdityaChopra18 <adityachopra1808@gmail.com>
2026-09-02 18:23:20 +0530

    Set app icon as favicon

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,10 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+export const links = () => [
+  { rel: "icon", href: "/favicon.png", type: "image/png" },
+];
+
 export default function App() {
   return (
     <html>
